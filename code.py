a61fd79589172cdf51ef7bf733926c12a3ef13fe
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-03-17 19:39:28 +0530

    Add candy sale input and validation logic

---
+++ code.py
@@ -0,0 +1,14 @@
+-- Solution:-
+
+sale=int(input("enter the no. of candies:"))
+
+if sale>0 and sale<=5:
+
+    print("NUMBER OF CANDIES SOLD:", sale)
+
+    print("NUMBER OF CANDIES AVAILABLE:",total-sale)
+    
+else:
+    print("INVALID INPUT")
+
+    print("NUMBER OF CANDIES AVAILABLE:",total)
